e54dde382d94f99be77e1dc5e73a5c0b357cf574
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:57:59 +0600

    ten

---
+++ 10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string number2Text = Convert.ToString(number);
+if (number2Text.Length > 2){
+  Console.WriteLine("третья цифра -> " + number2Text[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
