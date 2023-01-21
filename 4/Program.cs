59ee34b750daeebc733d3310b7d1d829e8609ffa
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:56:38 +0600

    four

---
+++ 4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать четные числа от 1 до N
+void even_n(int num1)
+{
+    int num2 = 2;
+    while (num2 <= num1)
+    {
+        Console.WriteLine(num2);
+        num2 = num2 + 2;
+    }
+}
+int n;
+Console.Write("Введите натуральное число: ");//5, 8
+n = Convert.ToInt32(Console.ReadLine());
+even_n(n);
