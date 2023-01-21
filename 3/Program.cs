6e69a760b5c21b6e836a625c4f50e934ab3dc895
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:56:24 +0600

    third

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Введите число ");
+int x;
+x = Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Ваше число чётное ");
+}
+else
+{
+    Console.WriteLine("Ваше число нечётное");
+}
