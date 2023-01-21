c22eb8c58739f07dfd17fa57c5d7923e3b61b30d
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:57:00 +0600

    six

---
+++ 6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Write("Text a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string number2Text = Convert.ToString(number);
+if (number2Text.Length > 2){
+  Console.WriteLine("second number is " + number2Text[1]);
+}
+else {
+  Console.WriteLine("no second number");
+}
