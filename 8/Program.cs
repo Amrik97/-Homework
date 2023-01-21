4ef5dd7953299314a5b3ea5c2b25590481f694e3
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:57:31 +0600

    eight

---
+++ 8/Program.cs
@@ -0,0 +1,10 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.Write("Text a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string number2Text = Convert.ToString(number);
+if (number2Text.Length > 2){
+  Console.WriteLine("second number is " + number2Text[0]+number2Text[2]);
+}
+else {
+  Console.WriteLine("no second number");
+}
