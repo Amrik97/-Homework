50d2f0902a7e5670debcbf92ba775cfba592aef1
Amirkhan <amirhan75@gmail.com>
2023-01-21 18:56:50 +0600

    five

---
+++ 5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Text a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string number2Text = Convert.ToString(number);
+if (number2Text.Length > 2){
+  Console.WriteLine("third number is " + number2Text[2]);
+}
+else {
+  Console.WriteLine("-> no third number");
+}
